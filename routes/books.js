3f51578d4791a6d54c74791d1e87dc345c41f575
unknown <vasvasanthanv@gmail.com>
2024-05-12 14:02:44 +0530

    Books json

--- routes/books.js
+++ routes/books.js
@@ -6,12 +6,23 @@ const router = Router();
 router.get('/',async(req,res) => {
     try{
         const books = await Books.find();
-        console.log(books);
         res.status(200).json({books});
     }
     catch(error){
         res.status(500).json({error : error});
     }
 })
+
+// router.get('/:name',async(req,res) => {
+//     const bookName = req.params.name;
+//     try{
+//         const books = await Books.find({ 'name' : bookName });
+//         console.log(books);
+//         res.status(200).json({books});
+//     }
+//     catch(error){
+//         res.status(500).json({error : error});
+//     }
+// })
 
 export default router;
