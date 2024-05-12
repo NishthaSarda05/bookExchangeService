8cb41291c99cd3f5341bf391ca1ef8740070f581
Monika2399 <moni.selva99@gmail.com>
2024-05-12 20:37:58 +0530

    Added schema changes.

--- models/Book.js
+++ models/Book.js
@@ -1,12 +1,20 @@
 import mongoose from 'mongoose';
 
 const BookSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  genre: { type: String, required: true },
-  condition: { type: String, required: true },
-  availability: { type: String, required: true },
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  id: { type: String },
+  name: { type: String },
+  price: {
+    raw: { type: Number },
+    formatted: { type: String },
+    formatted_with_symbol: { type: String },
+    formatted_with_code: { type: String },
+    },
+  media: {
+    type: { type: String },
+    source: { type: String },
+    },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
+
 
 module.exports = mongoose.model("Book", BookSchema);
